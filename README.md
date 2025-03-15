363de7b41b8d46f1338299c975e8845d45692b38
Georgi Georgiev <georgi.getz@outlook.com>
2025-03-15 16:53:42 +0200

    updated the readme file and added tests to the main functionality
    Signed-off-by: Georgi Georgiev <georgi.getz@outlook.com>

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # wasm-project
 
+Implementation of the pathfinding algorithm
+
 Prerequisites: Python
 
-Once you install python just run the following command
+Once you install python just run the following command:
+
 `python3 -m http.server 8080`
-And open your browser on localhost:8080 to vie the application
+
+and open your browser on localhost:8080 to view the application
+
+You can regenerate the contents of the pkg directory by running:
+
+`wasm-pack build --target web`
+
+You have to locally install `wasm-pack` if you want to do that
+
+lib.rs implements the pathfinding algorithm and the vast majority of the logic is written in the internal function
+so that it could be unit tested
--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,36 @@ use wasm_bindgen::prelude::*;
 use serde_wasm_bindgen::to_value;
 use std::collections::BinaryHeap;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 
 #[wasm_bindgen]
 pub fn find_path(start_x: i32, start_y: i32, goal_x: i32, goal_y: i32) -> JsValue {
-    let start = Node { x: start_x, y: start_y, cost: 0, heuristic: 0 };
+    let path = find_path_internal(start_x, start_y, goal_x, goal_y);
+    to_value(&path).unwrap()
+}
 
+pub fn find_path_internal(start_x: i32, start_y: i32, goal_x: i32, goal_y: i32) -> Vec<(i32, i32)> {
+    let start = Node { x: start_x, y: start_y, cost: 0, heuristic: 0 };
     let mut open_set = BinaryHeap::new();
     open_set.push(start);
-
-    let mut came_from = std::collections::HashMap::new();
-    let mut g_score = std::collections::HashMap::new();
+    let mut came_from = HashMap::new();
+    let mut g_score = HashMap::new();
     g_score.insert((start_x, start_y), 0);
-
     while let Some(current) = open_set.pop() {
         if current.x == goal_x && current.y == goal_y {
-            return to_value(&reconstruct_path(came_from, current)).unwrap();
+            return reconstruct_path(came_from, current);
         }
-
         for neighbor in get_neighbors(&current) {
             let tentative_g_score = g_score.get(&(current.x, current.y)).unwrap_or(&i32::MAX) + 1;
             if tentative_g_score < *g_score.get(&(neighbor.x, neighbor.y)).unwrap_or(&i32::MAX) {
                 came_from.insert((neighbor.x, neighbor.y), (current.x, current.y));
                 g_score.insert((neighbor.x, neighbor.y), tentative_g_score);
-                let heuristic = heuristic(neighbor.x, neighbor.y, goal_x, goal_y);
-                open_set.push(Node { x: neighbor.x, y: neighbor.y, cost: tentative_g_score, heuristic });
+                let h = heuristic(neighbor.x, neighbor.y, goal_x, goal_y);
+                open_set.push(Node { x: neighbor.x, y: neighbor.y, cost: tentative_g_score, heuristic: h });
             }
         }
     }
-
-    to_value(&Vec::<(i32, i32)>::new()).unwrap()
+    Vec::new()
 }
 
 #[derive(Copy, Clone, Eq, PartialEq)]
@@ -66,7 +67,7 @@ fn heuristic(x: i32, y: i32, goal_x: i32, goal_y: i32) -> i32 {
     (goal_x - x).abs() + (goal_y - y).abs()
 }
 
-fn reconstruct_path(mut came_from: std::collections::HashMap<(i32, i32), (i32, i32)>, current: Node) -> Vec<(i32, i32)> {
+fn reconstruct_path(mut came_from: HashMap<(i32, i32), (i32, i32)>, current: Node) -> Vec<(i32, i32)> {
     let mut path = vec![(current.x, current.y)];
     let mut current = (current.x, current.y);
     while let Some(prev) = came_from.remove(&current) {
@@ -75,4 +76,42 @@ fn reconstruct_path(mut came_from: std::collections::HashMap<(i32, i32), (i32, i
     }
     path.reverse();
     path
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_start_equals_goal() {
+        let path = find_path_internal(0, 0, 0, 0);
+        assert_eq!(path, vec![(0, 0)]);
+    }
+
+    #[test]
+    fn test_simple_path() {
+        let path = find_path_internal(0, 0, 1, 0);
+        assert_eq!(path, vec![(0, 0), (1, 0)]);
+    }
+
+    #[test]
+    fn test_diagonal_path() {
+        let path = find_path_internal(0, 0, 2, 2);
+        assert_eq!(path.first(), Some(&(0, 0)));
+        assert_eq!(path.last(), Some(&(2, 2)));
+        assert_eq!(path.len(), 5);
+        for pair in path.windows(2) {
+            let (x1, y1) = pair[0];
+            let (x2, y2) = pair[1];
+            assert_eq!((x1 - x2).abs() + (y1 - y2).abs(), 1);
+        }
+    }
+
+    #[test]
+    fn test_negative_coordinates() {
+        let path = find_path_internal(-1, -1, 1, 1);
+        assert_eq!(path.first(), Some(&(-1, -1)));
+        assert_eq!(path.last(), Some(&(1, 1)));
+        assert_eq!(path.len(), 5);
+    }
 }
